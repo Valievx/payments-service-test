235deb2170c455d5fa3c782e8fa92890c1bd9e9e
Alexander Valiev <worxvaliev@gmail.com>
2025-05-30 01:04:12 +0500

    add admin

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Organization, Payment
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'operation_id', 'amount', 'document_number',
+        'document_data', 'organization', 'created_at'
+    )
+    search_fields = ('operation_id',)
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'inn', 'balance')
+    search_fields = ('inn',)
+    readonly_fields = ('balance',)
+
+
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Organization, OrganizationAdmin)
